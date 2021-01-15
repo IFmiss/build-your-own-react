9452b5e547a0465bb10446d6044300bc0526640b
ifmiss <185098535@qq.com>
2021-01-15 11:52:36 +0800

    update: cy

--- src/Dedoo/createElement.ts
+++ src/Dedoo/createElement.ts
@@ -1,7 +1,28 @@
 function createElement(
-  type,
-  props,
+  type: DedooElementType,
+  props: object,
   ...children
-) {
+): DedooElement {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children?.map(
+        child =>
+          typeof child === 'object'
+            ? child
+            : createTextElement(child)
+      )
+    },
+  }
+}
 
+function createTextElement(child: string): DedooElement {
+  return {
+    type: 'TEXT_ELEMENT',
+    props: {
+      nodeValue: child,
+      children: []
+    }
+  };
 }
